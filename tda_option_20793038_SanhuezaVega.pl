bb15390101b21591321aa81a7e7dd60b4db39732
Alonso Sanhueza <alonso.sanhueza32@gmail.com>
2023-11-02 17:23:39 -0300

    Se ha modificado option, ahora convierte los todos los strings todo en minuscula.

--- tda_option_20793038_SanhuezaVega.pl
+++ tda_option_20793038_SanhuezaVega.pl
@@ -6,4 +6,27 @@
 % M. Primaria: option/6
 % M. Secundaria: -
 option(ID, Message, ChatbotCodeLink, FlowCodeLink, Keyword, Option):-
-    Option = [ID, Message, ChatbotCodeLink, FlowCodeLink, Keyword].
+    downcase_atom(Message, MinMessage),
+    stringDownInList(Keyword, MinKeyword),
+    Option = [ID, MinMessage, ChatbotCodeLink, FlowCodeLink, MinKeyword].
+
+% ######################################## MODIFICADOR #######################################
+
+% Descripcion: Convierte todos los strings de una lista en minusculas.
+% Dom: OriginalList (list) X FinalList (var)
+% M. Primaria: stringDownInList/2
+% M. Secundaria: stringDownInList/3
+stringDownInList(OriginalList, FinalList):-
+    stringDownInList(OriginalList, [], FinalList).
+
+% Descripcion: Auxiliar de stringDownInList/2, cambia cada string de la lista a minusculas.
+% Dom: OriginalList (list) X NewList (list) X FinalList (var)
+% M. Primaria: stringDownInList/3
+% M. Secundaria: downcase_atom/2, append/3
+stringDownInList([], NewList, FinalList):-
+    FinalList = NewList, !.
+
+stringDownInList([First | Rest], NewList, FinalList):-
+    downcase_atom(First, NewFirst),
+    append(NewList, [NewFirst], NewUpdatedList),
+    stringDownInList(Rest, NewUpdatedList, FinalList).
